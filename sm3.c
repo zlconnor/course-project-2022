3238693c692edf980527a97f5d26ed14b0eff4a7
zlconnor <zhanglinkangzlk@163.com>
2022-05-15 11:12:49 +0800

    update sm3 code

--- sm3.c
+++ sm3.c
@@ -1,175 +1,211 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 #include<malloc.h>
-/*
-梳理一下算法过程
-1. 填充m->m'
-2. 分组m'->B[0]...B[n-1] 512bit一组
-3. 迭代压缩 V(i+1)=CF(V[i],B[i])
 
 
-*/
+unsigned int IV[8] = { 0 };//initial vector
+unsigned int T[64] = { 0 };//T array
+unsigned char B[64] = { 0 };//block B(i)
 
 
-static unsigned int IV[8];
-static unsigned int T[64];
-void init_IV() {
-    IV[0] = 0x7380166f;
-    IV[1] = 0x4914b2b9;
-    IV[2] = 0x172442d7;
-    IV[3] = 0xda8a0600;
-    IV[4] = 0xa96f30bc;
-    IV[5] = 0x163138aa;
-    IV[6] = 0xe38dee4d;
-    IV[7] = 0xb0fb0e4e;
-}
-void init_T() {
-    int j = 0;
-    for (j = 0; j <= 15; j++) {
-        T[j] = 0x79cc4519;
-    }
-    for (j = 16; j <= 63; j++) {
-        T[j] = 0x7a879d8a;
-    }
-}
-unsigned int FF(unsigned int X, unsigned int Y, unsigned int Z, int j) {
-    int ret = 0;
-    if (j >= 0 && j <= 15) {
-        ret = X ^ Y ^ Z;
-    }
-    else if (j >= 16 && j <= 63) {
-        ret = (X & Y) | (X ^ Z) | (Y ^ Z);
-    }
-    return ret;
-}
-unsigned int GG(unsigned int X, unsigned int Y, unsigned int Z, int j) {
-    int ret = 0;
-    if (j >= 0 && j <= 15) {
-        ret = X ^ Y ^ Z;
-    }
-    else if (j >= 16 && j <= 63) {
-        ret = (X & Y) | (~X | Z);
-    }
-    return ret;
-}
-unsigned int left_cycle_shift(unsigned int value, int n) {
-    return (value >> (32 - n)) | (value << n);
-}
-unsigned int P_0(unsigned int X) {
-    return X ^ left_cycle_shift(X, 9) ^ left_cycle_shift(X, 17);
-}
-unsigned int P_1(unsigned int X) {
-    return X ^ left_cycle_shift(X, 15) ^ left_cycle_shift(X, 23);
-}
-void reverse_by_byte(char* const le, int len) {
-    for (int i = 0; i < len >> 1; i++) {
-        char tmp = le[i];
-        le[i] = le[len - 1 - i];
-        le[len - 1 - i] = tmp;
-    }
+unsigned int left_cycle_shift(unsigned int val, unsigned int n)//cycle shift left
+{
+	n = n % 32;
+	return ((val << n) & 0xFFFFFFFF) | ((val & 0xFFFFFFFF) >> (32 - n));
 }
 
-//借鉴 好好考虑一下
-char* padding(char* src, int src_len, int content_len, int* out_len) {
-    int l = sizeof(char) * src_len << 3;//m的原始长度bit
-    int content_bit_len = sizeof(char) * content_len << 3;
-    int k = (448 - ((l % 512) + 1) + 512) % 512;//需要补充的长度bit
-    int len_aft_padding = l + 1 + 64 + k;//padding后的长度bit
 
-    char* result = (char*)malloc(sizeof(char) * len_aft_padding / 8);
-    memset(result, 0, sizeof(result));
-    memcpy(result, src, sizeof(char) * src_len);
-    char one = 0x80;
-    result[src_len] = one;
-
-    char* big_content_bit_len = (char*)malloc(sizeof(char) << 3);
-    memcpy(big_content_bit_len, &content_bit_len, 8);
-    reverse_by_byte(big_content_bit_len, 8);
-    memcpy(&result[len_aft_padding - 8], big_content_bit_len, 8);
-    free(big_content_bit_len);
-    if (out_len != NULL) {
-        *out_len = len_aft_padding;
-    }
-    return result;
-}
-void msg_expansion(char* Bi) {
-    unsigned int W[64];
-    unsigned int W_p[64];
-    for (int j = 0; j < 16; j++) {
-        W[j] = Bi[4 * j + 0] << 24 | Bi[4 * j + 1] << 16 | Bi[4 * j + 2] << 8 | Bi[4 * j + 3];
-    }
-    for (int j = 16; j < 68; j++) {
-        W[j] = P_1(W[j - 16] ^ W[j - 9] ^ left_cycle_shift(W[j - 3], 15)) ^ left_cycle_shift(W[j - 13], 7) ^ W[j - 6];
-    }
-    for (int j = 0; j < 64; j++) {
-        W_p[j] = W[j] ^ W[j + 4];
-    }
-
-
+void init_T()                   //initialize the T array
+{
+	for (int j = 0; j <= 15; j++)
+	{
+		T[j] = 0x79cc4519;
+	}
+	for (int j = 16; j <= 63; j++)
+	{
+		T[j] = 0x7a879d8a;
+	}
 }
 
-    //分组
-void block(char* m_p, int l, int k) {
-    init_IV();
-    for (int i = 0; i < (l + k + 65) / 512; i++) {
-        CF(m_p + 512 * i);
-    }
+unsigned int FF(unsigned int X, unsigned int Y, unsigned int Z, int j)
+{
+	unsigned int ret = 0;
+	if (j >= 0 && j <= 15)
+	{
+		ret = X ^ Y ^ Z;
+	}
+	else if (j >= 16 && j <= 63)
+	{
+		ret = (X & Y) | (X & Z) | (Y & Z);
+	}
+	return ret;
 }
 
 
 
-void CF(char* Bi) {
-    unsigned int W[64];
-    unsigned int W_p[64];
-    for (int j = 0; j < 16; j++) {
-        W[j] = Bi[4 * j + 0] << 24 | Bi[4 * j + 1] << 16 | Bi[4 * j + 2] << 8 | Bi[4 * j + 3];
-    }
-    for (int j = 16; j < 68; j++) {
-        W[j] = P_1(W[J - 16] ^ W[j - 9] ^ left_cycle_shift(W[j - 3], 15)) ^ left_cycle_shift(W[j - 13], 7) ^ W[j - 6];
-    }
-    for (int j = 0; j < 64; j++) {
-        W_p[j] = W[j] ^ W[j + 4];
-    }
-    unsigned int A = IV[0];
-    unsigned int B = IV[1];
-    unsigned int C = IV[2];
-    unsigned int D = IV[3];
-    unsigned int E = IV[4];
-    unsigned int F = IV[5];
-    unsigned int G = IV[6];
-    unsigned int H = IV[7];
-    unsigned int SS1;
-    unsigned int SS2;
-    unsigned int TT1;
-    unsigned int TT2;
-    for (int j = 0; j < 64; j++) {
-        SS1 = left_cycle_shift(A, 12) + E + left_cycle_shift(left_cycle_shift(T[i], j % 32), 7);
-        SS2 = SS1 ^ left_cycle_shift(A, 12);
-        TT1 = FF(A, B, C, i) + D + SS2 + W_p[i];
-        TT2 = GG(E, F, G, i) + H + SS1 + W[i];
-        D = C;
-        C = left_cycle_shift(B, 9);
-        B = A;
-        A = TT1;
-        H = G;
-        G = left_cycle_shift(F, 19);
-        F = E;
-        E = P_0(TT2);
-    }
-    IV[0] = A ^ IV[0];
-    IV[1] = B ^ IV[1];
-    IV[2] = C ^ IV[2];
-    IV[3] = D ^ IV[3];
-    IV[4] = E ^ IV[4];
-    IV[5] = F ^ IV[5];
-    IV[6] = G ^ IV[6];
-    IV[7] = H ^ IV[7];
+unsigned int GG(unsigned int X, unsigned int Y, unsigned int Z, int j)
+{
+	unsigned int ret = 0;
+	if (j >= 0 && j <= 15)
+	{
+		ret = X ^ Y ^ Z;
+	}
+	else if (j >= 16 && j <= 63)
+	{
+		ret = (X & Y) | ((~X) & Z);
+	}
+	return ret;
+}
+
+unsigned int P_0(unsigned int X)
+{
+	return X ^ left_cycle_shift(X, 9) ^ left_cycle_shift(X, 17);
+}
+
+unsigned int P_1(unsigned int X)
+{
+	return X ^ left_cycle_shift(X, 15) ^ left_cycle_shift(X, 23);
 }
 
 
+void CF(unsigned char* Bi) //compress func
+{
+	unsigned int W[68];
+	unsigned int W_p[64];
+	for (int j = 0; j <= 15; j++)
+	{
+		W[j] = Bi[4 * j + 0] << 24 | Bi[4 * j + 1] << 16 | Bi[4 * j + 2] << 8 | Bi[4 * j + 3];
+	}
+	for (int j = 16; j <= 67; j++)
+	{
+		W[j] = P_1(W[j - 16] ^ W[j - 9] ^ left_cycle_shift(W[j - 3], 15)) ^ left_cycle_shift(W[j - 13], 7) ^ W[j - 6];
+	}
+	for (int j = 0; j <= 63; j++)
+	{
+		W_p[j] = W[j] ^ W[j + 4];
+	}
+	unsigned int A = IV[0];
+	unsigned int B = IV[1];
+	unsigned int C = IV[2];
+	unsigned int D = IV[3];
+	unsigned int E = IV[4];
+	unsigned int F = IV[5];
+	unsigned int G = IV[6];
+	unsigned int H = IV[7];
+	unsigned int SS1;
+	unsigned int SS2;
+	unsigned int TT1;
+	unsigned int TT2;
+	for (int j = 0; j <= 63; j++)
+	{
+		SS1 = left_cycle_shift((left_cycle_shift(A, 12) + E + left_cycle_shift(T[j], j % 32)) & 0xffffffff, 7);//modulo 32
+
+		SS2 = SS1 ^ left_cycle_shift(A, 12);
+		TT1 = (FF(A, B, C, j) + D + SS2 + W_p[j]) & 0xffffffff;//modulo 32
+		TT2 = (GG(E, F, G, j) + H + SS1 + W[j]) & 0xffffffff;//modulo 32
+		D = C;
+		C = left_cycle_shift(B, 9);
+		B = A;
+		A = TT1;
+		H = G;
+		G = left_cycle_shift(F, 19);
+		F = E;
+		E = P_0(TT2);
+	}
+	IV[0] = A ^ IV[0];
+	IV[1] = B ^ IV[1];
+	IV[2] = C ^ IV[2];
+	IV[3] = D ^ IV[3];
+	IV[4] = E ^ IV[4];
+	IV[5] = F ^ IV[5];
+	IV[6] = G ^ IV[6];
+	IV[7] = H ^ IV[7];
+}
+
+void init_IV()//initialize IV 32*8 256bit
+{
+
+	IV[0] = 0x7380166f;
+	IV[1] = 0x4914b2b9;
+	IV[2] = 0x172442d7;
+	IV[3] = 0xda8a0600;
+	IV[4] = 0xa96f30bc;
+	IV[5] = 0x163138aa;
+	IV[6] = 0xe38dee4d;
+	IV[7] = 0xb0fb0e4e;
+}
+
+void block_and_compress(unsigned char* m, int len) {//block and then into compress func
+	int byte_remain = 0;
+	unsigned long long bit_length = 0;
+
+	for (int i = 0; i < len / 64; i++) {//get len/64 blocks
+		memcpy(B, m + i * 64, 64);
+		CF(B);
+	}
+
+	bit_length = len * 8;
+	byte_remain = len % 64;
+	memset(&B[byte_remain], 0, 64 - byte_remain);
+	memcpy(B, m + (len/64)*64, byte_remain);
+	B[byte_remain] = 0x80;
+	if (byte_remain <= 55) {//padding and compression <=448
+		for (int i = 0; i < 8; i++)
+			B[56 + i] = (bit_length >> ((8 - 1 - i) * 8)) & 0xFF;
+		CF(B);
+	}
+	else {
+		CF(B); //>48 CF(B), then pad and CF(B)
+		memset(B, 0, 64);
+		for (int i = 0; i < 8; i++)
+			B[56 + i] = (bit_length >> ((8 - 1 - i) * 8)) & 0xFF;
+		CF(B);
+	}
+
+}
 
 
+void char2hex() {
+	for (int i = 0; i < 8; i++) {
+		printf("%08x ", IV[i]);
+	}
+	printf("\n");
+}
 
+void char2str(unsigned char* value) {
+	for (int i = 0; i < 8; i++) {
+		value[4 * i] = (IV[i] >> 24) & 0xff;
+		value[4 * i + 1] = (IV[i] >> 16) & 0xff;
+		value[4 * i + 2] = (IV[i] >> 8) & 0xff;
+		value[4 * i + 3] = IV[i] & 0xff;
+	}
+	for (int i = 0; i < 32; i++)
+		printf("%c", value[i]);
+	printf("\n");
+}
 
-
+int SM3(unsigned char* m, unsigned int len, unsigned char* hash_val)
+{
+	init_IV();
+	init_T();
+	block_and_compress(m, len);
+	char2hex();
+	char2str(hash_val);
+	return 1;
+}
+int main()
+{
+	unsigned char hash_value[32] = { 0 };
+	int len;
+	char* str = "abc";
+	//66c7f0f4 62eeedd9 d1f2d46b dc10e4e2 4167c487 5cf2f7a2 297da02b 8f4ba8e0
+	len = strlen(str);
+	if (!SM3(str, len, hash_value))
+		printf("1 false\n");
+	char* str2 = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";
+	//debe9ff9 2275b8a1 38604889 c18e5a4d 6fdb70e5 387e5765 293dcba3 9c0c5732
+	len = strlen(str2);
+	if (!SM3(str2, len, hash_value))
+		printf("2 false\n");
+	return 0;
+}
